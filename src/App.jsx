254e2ed04bc42bef1b265370f12167d7f4ddc693
Shree Gowda <shreegowda211@gmail.com>
2026-02-10 11:13:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-500">
+      Tailwind is working 🚀
+    </h1>
+  );
+}
+
+
+
